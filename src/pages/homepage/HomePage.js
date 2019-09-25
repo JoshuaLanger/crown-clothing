42a87b786855bdb2c373aa4eb5f688f0603a6f88
unknown <joshuarllanger@gmail.com>
2019-09-25 12:35:33 -0500

    init commit

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styles
+import './HomePage.scss';
+
+//Components
+import Directory from '../../components/directory/Directory';
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
